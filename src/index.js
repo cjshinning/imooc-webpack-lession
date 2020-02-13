4dc7876cf230a3b0e5722c0c9a5cc4a9f2082359
cjshinning <784319417@qq.com>
2020-02-13 11:31:49 +0800

    3-2 使用loader打包静态资源（样式篇下1）

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-// 使用loader可以帮助webpack打包除了javascript的其他静态资源
 import avatar from './avatar.jpg';
-import './index.scss';
+import style from './index.scss';
+import createAvatar from './createAvatar.js';
+
+createAvatar();
 
 var img = new Image();
 img.src = avatar;
-img.classList.add('avatar');
+img.classList.add(style.avatar);
 
 var root = document.getElementById('root');
 root.append(img);
