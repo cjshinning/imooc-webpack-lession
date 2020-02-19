9ab611d7dc4f4dc626cc95521be073a27086273d
cjshinning <784319417@qq.com>
2020-02-19 15:00:26 +0800

    4-5 SplitChunksPlugin 配置参数详解

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -64,9 +64,25 @@ module.exports = {
         }), 
         new CleanWebpackPlugin()
     ],
-    // optimization: {
-    //     splitChunks: {
-    //         chunks: 'all'
-    //     }
-    // }
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            minSize: 30000,
+            // minRemainingSize: 0,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 6,
+            maxInitialRequests: 4,
+            automaticNameDelimiter: '~',
+            automaticNameMaxLength: 30,
+            cacheGroups: {
+            defaultVendors: {
+                test: /[\\/]node_modules[\\/]/,
+                priority: -10,
+                filename: 'vendors.js'
+              },
+              default: false
+            }
+        }
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,35 +1,23 @@
-// 第一种方式
-// 首次访问页面时，加载main.js(2mb)
-// 当页面业务逻辑发生变化时，又要加载2mb的内容
+import _ from 'lodash';
 
-// 业务逻辑1mb
-// console.log(_.join(['a', 'd', 'c'], '***'));
-// 此处省略10万行业务逻辑
-// console.log(_.join(['a', 'b', 'c'], '***'));
+let element = document.createElement('div');
+element.innerHTML = _.join(['Jenny', 'Chan'], '-');
+document.body.appendChild(element);
 
-// 第二种方式
-// main.js被拆成lodash.js(1mb),main.js(1mb)
-// 当页面业务逻辑发生变化时，只需要加载main.js即可(1mb)
 
-// Code Spliting
-
-// 同步方式
-// import _ from 'lodash';
-// console.log(_.join(['a', 'd', 'c'], '***'));
-// console.log(_.join(['a', 'b', 'c'], '***'));
 
 // 异步方式
-function getComponent(){
-    return import('lodash').then(({default: _}) => {
-        let element = document.createElement('div');
-        element.innerHTML = _.join(['Jenny', 'Chan'], '-');
-        return element;
-    })
-}
+// function getComponent(){
+//     return import(/* webpackChunkName:'lodash' */ 'lodash').then(({default: _}) => {
+//         let element = document.createElement('div');
+//         element.innerHTML = _.join(['Jenny', 'Chan'], '-');
+//         return element;
+//     })
+// }
 
-getComponent().then(element => {
-    document.body.appendChild(element);
-})
+// getComponent().then(element => {
+//     document.body.appendChild(element);
+// })
 
 // 代码分割，和webpack无关
 // webpack实现代码分割，两种方式
