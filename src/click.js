9920cbfe9b21a1946b0c14aee82bcf46850e0882
cjshinning <784319417@qq.com>
2020-02-23 13:01:48 +0800

    4-8 打包分享，preloading，prefetching

---
+++ src/click.js
@@ -0,0 +1,7 @@
+function handleClick(){
+    let element = document.createElement('div');
+    element.innerHTML = 'Jenny Chan';
+    document.body.appendChild(element);
+}
+
+export default handleClick;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-
-// import _ from 'lodash';
-
-// let element = document.createElement('div');
-// element.innerHTML = _.join(['Jenny', 'Chan'], '-');
-// document.body.appendChild(element);
-
-
-
-// 异步方式
-async function getComponent(){
-    const { default: _ } = await import(/* webpackChunkName:'lodash' */ 'lodash');
-    let element = document.createElement('div');
-    element.innerHTML = _.join(['Jenny', 'Chan'], '-');
-    return element;
-}
-
 document.addEventListener('click', ()=>{
-    getComponent().then(element => {
-        document.body.appendChild(element);
+    import(/* webpackPrefetch: true */ './click.js').then(({default: func}) => {
+        func();
     })
 })
 
