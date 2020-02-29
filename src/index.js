d35d39c751e201bff2e446b1eb66e8e61aec9aaa
cjshinning <784319417@qq.com>
2020-02-29 19:08:52 +0800

    5-8 webpack性能优化（2）

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log(this);
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Child from 'child';
+
+class App extends Component{
+    render() {
+        return (
+            <div>
+                <div>This is App</div>
+                <Child />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
