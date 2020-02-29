5abc9c90c16543b51330ed68a086aff449e020b6
cjshinning <784319417@qq.com>
2020-02-29 13:55:23 +0800

    5-7 EsLint在webpack中的配置3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
@@ -19,28 +18,3 @@ class App extends Component {
 }
 
 ReactDom.render(<App />, document.getElementById('root'));
-=======
-console.log('Hello, this is Jenny');
-
-// if('serviceWorker' in navigator){
-//     window.addEventListener('load', () => {
-//         navigator.serviceWorker.register('/service-worker.js')
-//             .then(registration => {
-//                 console.log('service-worker registed');
-//             }).catch(error => {
-//                 console.log('service-worker register error');
-//             })
-//     })
-// }
-
-
-if ('serviceWorker' in window.navigator) {
-    navigator.serviceWorker.register('/dist/service-worker.js')
-    .then(function (reg) {
-        console.log('success', reg);
-    })
-    .catch(function (err) {
-        console.log('fail', err);
-    });
-}  
->>>>>>> c1fe8ffbb8879591de6abb5fd42a7aca634499d7
